b7ea472a41b8724cc3c04ea81499ed787d510259
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-12 17:16:16 -0400

    Define constructor and accessors for rationals

--- ch2.rkt
+++ ch2.rkt
@@ -21,3 +21,15 @@
 (define (equal-rat? x y)
   (= (* (numer x) (denom y))
      (* (numer y) (denom x))))
+
+(define (make-rat n d) (cons n d))
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
