92b5db1ce6378d8223d13f4375e76a17c5d90c59
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-13 12:02:06 -0400

    Expand (add-1 zero) add printing function

--- church.rkt
+++ church.rkt
@@ -2,5 +2,18 @@
 
 (define zero (lambda (f) (lambda (x) x)))
 
+(define one
+  (lambda (f)
+    (lambda (x)
+      (f (((lambda (f)
+             (lambda (x) x)) f) x)))))
+
 (define (add-1 n)
-  (lambda (f) (lambda (x) (f ((n f) x)))))
+  (lambda (f)
+    (lambda (x)
+      (f ((n f) x)))))
+
+
+;; (add-1 zero)
+;; (add-1 (lambda (f) (lambda (x) x)))
+;; (lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) x)) f) x))))
