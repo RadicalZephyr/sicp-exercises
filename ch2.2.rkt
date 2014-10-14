ad42f6a1a7fdbd1c2f9b487226bb4881c8843df6
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-14 12:45:05 -0400

    Exercise 2.30

--- ch2.2.rkt
+++ ch2.2.rkt
@@ -204,3 +204,18 @@
              (scale-tree sub-tree factor)
              (* sub-tree factor)))
        tree))
+
+;; Ex. 2.30
+
+(define (square-tree tree)
+  (cond [(null? tree)       nil]
+        [(not (pair? tree)) (square tree)]
+        [else               (cons (square-tree (car tree))
+                                  (square-tree (cdr tree)))]))
+
+(define (square-treem tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-treem sub-tree)
+             (square sub-tree)))
+       tree))
