eff50cf46edd388941adda67b9539814c92df0e3
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-14 11:49:59 -0400

    Exercise 2.19

--- ch2.2.rkt
+++ ch2.2.rkt
@@ -36,3 +36,33 @@
         accum
         (rev-it (cons (car items) accum) (cdr items))))
   (rev-it '() items))
+
+
+;; Ex. 2.19
+
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination coin-values))
+            (cc (- amount
+                   (first-denomination coin-values))
+                coin-values)))))
+
+(define (first-denomination values)
+  (car values))
+
+(define (except-first-denomination values)
+  (cdr values))
+
+(define (no-more? values)
+  (null? values))
+
+;; The order of coin-values doesn't matter.  This is unexpected.  My
+;; intuition was that the order should matter, but I guess because the
+;; recursion proceeds to the point of using smallest coins only,
+;; putting the smallest coins first doesn't make any difference.
