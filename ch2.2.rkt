ac585923f9d71d263b402c3a7b21097b5cce055d
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-14 14:03:52 -0400

    Exercise 2.33

--- ch2.2.rkt
+++ ch2.2.rkt
@@ -234,3 +234,26 @@
       initial
       (op (car sequence)
           (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+(define (map-a p sequence)
+  (accumulate (lambda (x y)
+                (cons (p x)
+                      y))
+              nil sequence))
+
+(define (append-a seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length-a sequence)
+  (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
