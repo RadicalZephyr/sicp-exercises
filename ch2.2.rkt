65c2dee1689894803c6156a7aecddc6883b183c0
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-14 12:52:41 -0400

    Add filter and accumulate from text

--- ch2.2.rkt
+++ ch2.2.rkt
@@ -219,3 +219,18 @@
              (square-treem sub-tree)
              (square sub-tree)))
        tree))
+
+;; from the text
+
+(define (filter predicate sequence)
+  (cond [(null? sequence) nil]
+        [(predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence)))]
+        [else (filter predicate (cdr sequence))]))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
