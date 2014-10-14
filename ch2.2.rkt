52c564ad0751e41857796b090706c2731743ab52
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-14 11:42:51 -0400

    Start working on 2.2 exercises

---
+++ ch2.2.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (- n 1))))
+
+(define (length items)
+  (if (null? items)
+      0
+      (+ 1 (length (cdr items)))))
+
+(define (lengthi items)
+  (define (length-iter a count)
+    (if (null? a)
+        count
+        (length-iter (cdr a) (+ 1 count))))
+  (length-iter items 0))
+
+(define (append list1 list2)
+  (if (null? list1)
+      list2
+      (cons (car list1) (append (cdr list1) list2))))
+
+;; Ex. 2.17
+(define (last-pair items)
+  (if (null? (cdr items))
+      items
+      (last-pair (cdr items))))
